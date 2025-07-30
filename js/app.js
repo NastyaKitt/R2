75fc0101e6a6f01453ef74a4a0c272d50079077f
NastyaKitt <Vbloger23@gmail.com>
2025-07-30 13:40:21 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 function checkVisibility() {
   const elements = document.querySelectorAll(
-    '.blackKnight, .fourMan, .tableTwo, .tableTwo img, .tableTwo video, ' +
-    '.threeText, .tableThree, .tableThree img, .fourItem, .fourItem img, ' +
-    '.fourTable, .fourTable img, .tableFive, .tableFive img, .dragons, .fiveBlockButton'
+    '.blackKnight, .fiveMan, .tableTwo, .tableTwo img, .tableTwo video, ' +
+    '.threeText, .threeTextChine,.fourMan,.fourItem, .tableThree, .tableThree img, .fiveItem, .fiveItem img, ' +
+    '.fiveTable, .fiveTable img, .tableSix, .tableSix img, .dragons, .sixBlockButton'
   );
   const windowBottom = window.innerHeight;
 
@@ -16,3 +16,20 @@ function checkVisibility() {
 
 window.addEventListener('scroll', checkVisibility);
 window.addEventListener('load', checkVisibility);
+
+// 
+document.getElementById('scrollToTop').addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+document.getElementById('scrollToTop2').addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+// 
+
+document.querySelectorAll('.navItem').forEach(item => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
